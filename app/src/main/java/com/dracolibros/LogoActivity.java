b4acbdc2a47b9327d5e6c28c44f755e2d6a86fe3
joroge4242 42 <jorge.trabajo42@gmail.com>
2020-10-29 13:15:10 +0100

    Initial commit

---
+++ app/src/main/java/com/dracolibros/LogoActivity.java
@@ -0,0 +1,27 @@
+package com.dracolibros;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class LogoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logo);
+
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+}
